f9a2721a759a25550b6c906ebc073e031ba733a3
Derek Hammond <derek@optimizely.com>
2019-11-20 00:15:56 -0600

    update templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ exports.createPages = async ({ actions: { createPage } }) => {
   // Create a page that lists all Pokémon.
   createPage({
     path: `/`,
-    component: require.resolve('./src/templates/all-pokemon.js'),
+    component: require.resolve('./src/templates/home.js'),
     context: { datafile }
   });
 };
--- src/templates/home.js
+++ src/templates/home.js
@@ -7,34 +7,24 @@ import {
 } from '@optimizely/react-sdk'
 
 export default ({ pageContext: { datafile } }) => {
-  // Instantiate an Optimizely client
   const optimizely = createInstance({
     datafile,
   });
-
-  console.log(optimizely);
 
   return (
     <OptimizelyProvider
       optimizely={ optimizely }
       user={ { id: 'bartSimpson' } }>
       <div style={{ width: 960, margin: '4rem auto' }}>
-        <h1>Hello world!</h1>
+        <h1>My feature!</h1>
         <OptimizelyFeature feature="gatsby_react_feature">
-          { (isEnabled, variables) => {
-             console.log('isEnabled', isEnabled);
-             console.log('variables', variables);
-             return (
-               <div>
-                Enabled: { `${isEnabled}` }<br />
-                Variables: { JSON.stringify(variables, null, 2) }<br />
-                 { isEnabled
-                  ? `CTA IS ${variables.cta}`
-                  : `Sorry we're closed!`
-                 }
-               </div>
-            );
-          } }
+          { (isEnabled, variables) => (
+             <div>
+              Enabled: { `${isEnabled}` }<br />
+              Variables: { JSON.stringify(variables, null, 2) }<br />
+              CTA IS: { isEnabled ? variables.cta : `Sorry we're closed!` }
+             </div>
+          ) }
         </OptimizelyFeature>
         <h2>Your datafile:</h2>
         <pre>{ JSON.stringify(datafile, null, 2) }</pre>
