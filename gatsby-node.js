a239c99a7263712c8399fd2f117fc3b14dcb1501
Derek Hammond <derek@optimizely.com>
2019-11-19 23:14:05 -0600

    use Optimizely React SDK

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,12 @@
 const axios = require('axios');
 
-const get = endpoint => axios.get(`https://pokeapi.co/api/v2${endpoint}`);
-
-const getPokemonData = names =>
-  Promise.all(
-    names.map(async name => {
-      const { data: pokemon } = await get(`/pokemon/${name}`);
-      const abilities = await Promise.all(
-        pokemon.abilities.map(async ({ ability: { name: abilityName } }) => {
-          const { data: ability } = await get(`/ability/${abilityName}`);
-
-          return ability;
-        })
-      );
-
-      return { ...pokemon, abilities };
-    })
-  );
-
 exports.createPages = async ({ actions: { createPage } }) => {
-  const allPokemon = await getPokemonData(['pikachu', 'charizard', 'squirtle']);
+  const { data: datafile } = await axios.get('https://cdn.optimizely.com/datafiles/Ua55wZQEBEkSHLPreu6HYt.json');
 
   // Create a page that lists all Pokémon.
   createPage({
     path: `/`,
     component: require.resolve('./src/templates/all-pokemon.js'),
-    context: { allPokemon }
-  });
-
-  // Create a page for each Pokémon.
-  allPokemon.forEach(pokemon => {
-    createPage({
-      path: `/pokemon/${pokemon.name}/`,
-      component: require.resolve('./src/templates/pokemon.js'),
-      context: { pokemon }
-    });
-
-    // Create a page for each ability of the current Pokémon.
-    pokemon.abilities.forEach(ability => {
-      createPage({
-        path: `/pokemon/${pokemon.name}/ability/${ability.name}/`,
-        component: require.resolve('./src/templates/ability.js'),
-        context: { pokemon, ability }
-      });
-    });
+    context: { datafile }
   });
 };
--- src/templates/all-pokemon.js
+++ src/templates/all-pokemon.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import {
+ createInstance,
+ OptimizelyFeature,
+ OptimizelyProvider,
+} from '@optimizely/react-sdk'
 
-export default ({ pageContext: { allPokemon } }) => (
-  <div style={{ width: 960, margin: '4rem auto' }}>
-    <h1>Choose a Pokémon!</h1>
-    <ul style={{ padding: 0 }}>
-      {allPokemon.map(pokemon => (
-        <li
-          key={pokemon.id}
-          style={{
-            textAlign: 'center',
-            listStyle: 'none',
-            display: 'inline-block'
-          }}
-        >
-          <Link to={`/pokemon/${pokemon.name}`}>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <p>{pokemon.name}</p>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+export default ({ pageContext: { datafile } }) => {
+  // Instantiate an Optimizely client
+  const optimizely = createInstance({
+    datafile,
+  });
+
+  console.log(optimizely);
+
+  return (
+    <OptimizelyProvider
+      optimizely={ optimizely }
+      user={ { id: 'bartSimpson' } }>
+      <div style={{ width: 960, margin: '4rem auto' }}>
+        <h1>Hello world!</h1>
+        <OptimizelyFeature feature="gatsby_react_feature">
+          { (isEnabled, variables) => {
+             console.log('isEnabled', isEnabled);
+             console.log('variables', variables);
+             return (
+               <div>
+                Enabled: { `${isEnabled}` }<br />
+                Variables: { JSON.stringify(variables, null, 2) }<br />
+                 { isEnabled
+                  ? `CTA IS ${variables.cta}`
+                  : `Sorry we're closed!`
+                 }
+               </div>
+            );
+          } }
+        </OptimizelyFeature>
+        <h2>Your datafile:</h2>
+        <pre>{ JSON.stringify(datafile, null, 2) }</pre>
+      </div>
+    </OptimizelyProvider>
+  );
+};
